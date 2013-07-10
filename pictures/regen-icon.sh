516091cd9e769f6f3d2bc9f7da5bda0028de86a3
Michael Howell <michael@notriddle.com>
2013-07-10 15:52:08 -0700

    Forgot to include a license.

--- pictures/regen-icon.sh
+++ pictures/regen-icon.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+#
+# This file is a part of Budget with Envelopes.
+# Copyright 2013 Michael Howell <michael@notriddle.com>
+#
+# Budget is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Budget is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Budget. If not, see <http://www.gnu.org/licenses/>.
+
 inkscape -e ../res/drawable-mdpi/ic_launcher.png -w 48 -h 48 -z ic_launcher.svg
 inkscape -e ../res/drawable-hdpi/ic_launcher.png -w 72 -h 72 -z ic_launcher.svg
 inkscape -e ../res/drawable-xhdpi/ic_launcher.png -w 96 -h 96 -z ic_launcher.svg
