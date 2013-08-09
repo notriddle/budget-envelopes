510252c0b8b9c62b5aa90e014f63c367dd63810b
Anatolij Zelenin <az@azapps.de>
2013-08-09 18:21:58 +0200

    Improved PIN

--- src/com/notriddle/budget/PinActivity.java
+++ src/com/notriddle/budget/PinActivity.java
@@ -5,43 +5,61 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
 public class PinActivity extends Activity {
+	private EditText pin;
+	private SharedPreferences preferences;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		final SharedPreferences preferences = PreferenceManager
+		preferences = PreferenceManager
 				.getDefaultSharedPreferences(getApplicationContext());
 		if (preferences.getString("pin", "").equals("")) {
 			start();
 		}
 
 		setContentView(R.layout.activity_pin);
-		
-		
-		final EditText pin=(EditText) findViewById(R.id.pin);
-		Button pinEnter=(Button) findViewById(R.id.pinEnter);
+
+		pin = (EditText) findViewById(R.id.pin);
+		pin.setOnEditorActionListener(new OnEditorActionListener() {
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					check();
+					return true;
+				}
+				return false;
+			}
+		});
+		Button pinEnter = (Button) findViewById(R.id.pinEnter);
 		pinEnter.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Log.e("Blubb",preferences.getString("pin", ""));
-				if(pin.getText().toString().equals(preferences.getString("pin", "")))				{
-					start();
-				} else {
-					Toast.makeText(getApplicationContext(), R.string.pin_wrong, Toast.LENGTH_LONG).show();
-				}
+				check();
 			}
 		});
 
+	}
+
+	private void check() {
+		if (pin.getText().toString().equals(preferences.getString("pin", ""))) {
+			start();
+		} else {
+			Toast.makeText(getApplicationContext(), R.string.pin_wrong,
+					Toast.LENGTH_LONG).show();
+		}
 	}
 
 	private void start() {
--- src/com/notriddle/budget/SettingsActivity.java
+++ src/com/notriddle/budget/SettingsActivity.java
@@ -1,22 +1,27 @@
 package com.notriddle.budget;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.text.InputType;
 
 public class SettingsActivity extends PreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
-    }
-    public static class SettingsFragment extends PreferenceFragment
-    {
-        @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings);
-        }
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction()
+				.replace(android.R.id.content, new SettingsFragment()).commit();
+	}
+
+	public static class SettingsFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(final Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.settings);
+
+			EditTextPreference pref = (EditTextPreference) findPreference("pin");
+			pref.getEditText().setInputType(InputType.TYPE_CLASS_NUMBER);
+		}
+	}
 }
